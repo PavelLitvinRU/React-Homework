0aa76219f6c8617041e93d27490f456188d68c29
PavelLitvinRU <litvin.pv@gmail.com>
2018-03-29 23:15:38 +0300

    task 1,2,4
    Настроил webpack, подключил все зависимости, собрал проект. Добавил модуль с классом, по которому выводится имя разработчика.

---
+++ lesson-2/src/Developer.js
@@ -0,0 +1,6 @@
+export default class Developer {
+    constructor(fullname) {
+        this.name = fullname;
+
+    }
+};
---
+++ lesson-2/src/index.js
@@ -0,0 +1,5 @@
+import Developer from './Developer';
+
+const devName = new Developer("Alex Pupkin");
+
+document.write(`Привет, ${devName.name}`);
